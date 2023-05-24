60842895e75a003ace0b0fc3616db83f5cb87205
Pavel <tr.pav.ar@gmail.com>
2023-05-25 01:00:01 +0300

    feat: implement vacancy details

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,11 @@
 import styles from "./Logo.module.scss";
 import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 function Logo() {
+  const navigate = useNavigate();
   return (
-    <div className={styles.logo}>
+    <div onClick={() => navigate("/job-service")} className={styles.logo}>
       <img src={logo} alt="logo" />
       <span className={styles.logo_text}>Jobored</span>
     </div>
